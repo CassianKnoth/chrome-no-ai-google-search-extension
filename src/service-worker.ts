8267bb9c8a08ca4a2b730428541c39e345b7a829
CassianKnoth <cassian.knoth@accenture.com>
2026-01-09 17:57:05 +0100

    DNR-roadblock

---
+++ src/service-worker.ts
@@ -0,0 +1,24 @@
+chrome.runtime.onInstalled.addListener(async () => {
+	await chrome.declarativeNetRequest.updateDynamicRules({
+		removeRuleIds: [1],
+		addRules: [
+			{
+				id: 1,
+				priority: 1,
+				action: {
+					type: 'redirect',
+					redirect: {
+						regexSubstitution:
+							'chrome-extension://' +
+							chrome.runtime.id +
+							'/helper-redirect.html?\\1',
+					},
+				},
+				condition: {
+					regexFilter: '^https://www\\.google\\.com/search\\?(.*)',
+					resourceTypes: ['main_frame'],
+				},
+			},
+		],
+	});
+});
