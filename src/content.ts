837a49d5a32438fdf022970790def85d01a1cf26
CassianKnoth <cassian.knoth@accenture.com>
2026-02-19 13:13:31 +0100

    this works

---
+++ src/content.ts
@@ -0,0 +1,13 @@
+(function () {
+	if (new URLSearchParams(location.search).has('noai')) {
+		return;
+	}
+
+	const url = new URL(location.href);
+	const q = url.searchParams.get('q');
+
+	url.searchParams.set('q', `${q} -ai`);
+	url.searchParams.set('noai', 'true');
+
+	location.replace(url.toString());
+})();
